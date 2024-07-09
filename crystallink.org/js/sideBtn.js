631b9c9ad692a98181ba353c98e776cf2cca9b6c
Emmanuel Ochigbo <ochigboje@gmail.com>
2024-07-09 18:03:48 +0100

    major overhaul of the hero section

--- crystallink.org/js/sideBtn.js
+++ crystallink.org/js/sideBtn.js
@@ -4,7 +4,7 @@
     window.onscroll = function () { scrollFunction() };
   
     function scrollFunction() {
-      if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+      if (document.body.scrollTop >900 || document.documentElement.scrollTop > 900) {
         var sideButtons = document.getElementsByClassName("sideBtn");
         for (var i = 0; i < sideButtons.length; i++) {
           sideButtons[i].style.display = "block";
