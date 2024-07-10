6a589ace548086250bab85b1490f9757046db4bd
Emmanuel Ochigbo <ochigboje@gmail.com>
2024-07-10 14:41:20 +0100

    completed the restructuring of the Hero section of crystallink.org

--- crystallink.org/js/sideBtn.js
+++ crystallink.org/js/sideBtn.js
@@ -4,7 +4,7 @@
     window.onscroll = function () { scrollFunction() };
   
     function scrollFunction() {
-      if (document.body.scrollTop >900 || document.documentElement.scrollTop > 900) {
+      if (document.body.scrollTop >1400 || document.documentElement.scrollTop > 1400) {
         var sideButtons = document.getElementsByClassName("sideBtn");
         for (var i = 0; i < sideButtons.length; i++) {
           sideButtons[i].style.display = "block";
@@ -22,3 +22,28 @@
       document.body.scrollTop = 0; // For Safari
       document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     }
+
+
+    // image in hero section
+    window.addEventListener('scroll', function() {
+      var scrollPosition = window.scrollY;
+      var imageElement = document.querySelector('.bg-img-hero');
+      
+      // Adjust the scaling factor based on scroll position
+      var scaleFactor = 1 + (scrollPosition / 3000);
+      imageElement.style.transform = 'scale(' + scaleFactor + ')';
+  });
+
+
+
+
+  window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY;
+    var items = document.querySelectorAll('.hero-ads-item');
+    
+    items.forEach(function(item) {
+        // Adjust the scaling factor based on scroll position
+        var scaleFactor = 1 + (scrollPosition / 1000);
+        item.style.transform = 'scale(' + scaleFactor + ')';
+    });
+});
