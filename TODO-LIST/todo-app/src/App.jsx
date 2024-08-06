8591eaa86dd255c42d827b40b115cf5ee4a7376c
Emmanuel Ochigbo <ochigboje@gmail.com>
2024-08-06 01:20:27 +0100

    added dynamic border and background to index.html of crystallink,org, and added more lines of code to TO-DO LIST

--- TODO-LIST/todo-app/src/App.jsx
+++ TODO-LIST/todo-app/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Todo from './components/Todo'
 
 const App = () => {
   return (
     <div>
-        
-      
+      {/* <h3>Testing</h3> */}
+      <Todo/>
     </div>
   )
 }
--- TODO-LIST/todo-app/src/components/Todo.jsx
+++ TODO-LIST/todo-app/src/components/Todo.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import { useEffect, useRef, useState } from 'react';
+import './Todo.css';
 
-const todo = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+function Todo() {
+    const [count, setCount] = useState(0);
+    const [todos, setTodos] = useState([]);
+    const inputRef = useRef(null);
+
+    function add() {
+        setTodos([...todos, { no: count, text: inputRef.current.value, display: "" }]);
+        setCount(count + 1);
+        inputRef.current.value = "";
+    }
+
+    useEffect(() => {
+        console.log(todos);
+    }, [todos]);
+
+    return (
+        <>
+            <div className="todo">
+                <div className="todo-header">TO-DO LIST</div>
+                <div className="todo-add">
+                    <input type="text" className='todo-input' placeholder='Enter Your Task' ref={inputRef} />
+                    <div onClick={add} className="todo-add-btn">ADD</div>
+                </div>
+                <div className="todo-list">
+                    {todos.map(todo => (
+                        <div key={todo.no} className="todo-item">
+                            {todo.text}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default todo
+export default Todo;
